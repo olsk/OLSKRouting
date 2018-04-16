8205526b18c3ea6803ead18732346c76dbb61163
Rosano Coutinho <pub@xeari.com>
2018-04-16 11:34:51 -0400

    moving files to new prefix

---
+++ main-tests.js
@@ -0,0 +1,121 @@
+/*!
+ * OldSkool
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+var assert = require('assert');
+var kConstants = require('../kConstants/testing.main').ROCOTestingConstants();
+
+var routingLibrary = require('./main');
+
+describe('OLSKRoutingInputDataIsRouteObject', function testOLSKRoutingInputDataIsRouteObject () {
+
+	it('returns false if not object', function () {
+		assert.strictEqual(routingLibrary.OLSKRoutingInputDataIsRouteObject(null), false);
+	});
+	
+	it('returns false if OLSKRoutePath not string', function () {
+		assert.strictEqual(routingLibrary.OLSKRoutingInputDataIsRouteObject(Object.assign(kConstants.OLSKTestingRouteObjectValid(), {
+			OLSKRoutePath: null,
+		})), false);
+	});
+	
+	describe('redirects', function () {
+
+		it('returns false if OLSKRouteRedirect not string', function () {
+			assert.strictEqual(routingLibrary.OLSKRoutingInputDataIsRouteObject(Object.assign(kConstants.OLSKTestingRouteObjectValidRedirect(), {
+				OLSKRouteRedirect: null,
+			})), false);
+		});
+
+		it('returns true', function () {
+			assert.strictEqual(routingLibrary.OLSKRoutingInputDataIsRouteObject(kConstants.OLSKTestingRouteObjectValidRedirect()), true);
+		});
+
+	});
+	
+	it('returns false if OLSKRouteMethod not string', function () {
+		assert.strictEqual(routingLibrary.OLSKRoutingInputDataIsRouteObject(Object.assign(kConstants.OLSKTestingRouteObjectValid(), {
+			OLSKRouteMethod: null,
+		})), false);
+	});
+	
+	it('returns false if OLSKRouteFunction not function', function () {
+		assert.strictEqual(routingLibrary.OLSKRoutingInputDataIsRouteObject(Object.assign(kConstants.OLSKTestingRouteObjectValid(), {
+			OLSKRouteFunction: null,
+		})), false);
+	});
+	
+	it('returns true if valid routeObject', function () {
+		assert.strictEqual(routingLibrary.OLSKRoutingInputDataIsRouteObject(kConstants.OLSKTestingRouteObjectValid()), true);
+	});
+
+});
+
+describe('OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams', function testOLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams () {
+
+	it('throws error if param1 not routeObject', function () {
+		assert.throws(function () {
+			routingLibrary.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams(null);
+		}, /OLSKErrorInputInvalid/);
+	});
+
+	it('returns path', function () {
+		assert.strictEqual(routingLibrary.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams(kConstants.OLSKTestingRouteObjectValid()), '/alpha');
+	});
+
+	it('returns localized path with OLSKRoutingLanguage', function () {
+		assert.strictEqual(routingLibrary.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams(kConstants.OLSKTestingRouteObjectValid(), {
+			OLSKRoutingLanguage: 'en'
+		}), '/en/alpha');
+	});
+
+	describe('when route path has params', function () {
+
+		it('throws error if param2 not object', function () {
+			assert.throws(function () {
+				routingLibrary.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams(Object.assign(kConstants.OLSKTestingRouteObjectValid(), {
+					OLSKRoutePath: '/alpha/:bravo',
+				}));
+			}, /OLSKErrorInputInvalid/);
+		});
+
+		it('throws error if param2 without matching single param', function () {
+			assert.throws(function () {
+				routingLibrary.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams(Object.assign(kConstants.OLSKTestingRouteObjectValid(), {
+					OLSKRoutePath: '/alpha/:bravo',
+				}), {});
+			}, /OLSKErrorInputInvalid/);
+		});
+
+		it('returns path with single param substituted', function () {
+			assert.strictEqual(routingLibrary.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams(Object.assign(kConstants.OLSKTestingRouteObjectValid(), {
+					OLSKRoutePath: '/alpha/:bravo',
+				}), {
+				bravo: 'charlie',
+			}), '/alpha/charlie');
+		});
+
+		it('throws error if param2 without matching multiple params', function () {
+			assert.throws(function () {
+				routingLibrary.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams(Object.assign(kConstants.OLSKTestingRouteObjectValid(), {
+					OLSKRoutePath: '/alpha/:bravo/:delta',
+				}), {
+				bravo: 'charlie',
+			});
+			}, /OLSKErrorInputInvalid/);
+		});
+
+		it('returns path with multiple params substituted', function () {
+			assert.strictEqual(routingLibrary.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams(Object.assign(kConstants.OLSKTestingRouteObjectValid(), {
+					OLSKRoutePath: '/alpha/:bravo/:delta',
+				}), {
+				bravo: 'charlie',
+				delta: 'echo'
+			}), '/alpha/charlie/echo');
+		});
+
+	});
+
+});
---
+++ main.js
@@ -0,0 +1,68 @@
+/*!
+ * OldSkool
+ * Copyright(c) 2018 Rosano Coutinho
+ * MIT Licensed
+ */
+
+//_ OLSKRoutingInputDataIsRouteObject
+
+exports.OLSKRoutingInputDataIsRouteObject = function (inputData) {
+	if (typeof inputData !== 'object' || inputData === null) {
+		return false;
+	};
+
+	if (typeof inputData.OLSKRoutePath !== 'string') {
+		return false;
+	};
+
+	if (inputData.OLSKRouteRedirect !== undefined) {
+		if (typeof inputData.OLSKRouteRedirect !== 'string') {
+			return false;
+		};
+
+		return true;
+	};
+
+	if (typeof inputData.OLSKRouteMethod !== 'string') {
+		return false;
+	};
+
+	if (typeof inputData.OLSKRouteFunction !== 'function') {
+		return false;
+	};
+
+	return true;
+};
+
+//_ OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams
+
+exports.OLSKRoutingCanonicalPathWithRouteObjectAndOptionalParams = function (routeObject, optionalParams) {
+	if (!exports.OLSKRoutingInputDataIsRouteObject(routeObject)) {
+		throw new Error('OLSKErrorInputInvalid');
+	};
+
+	var canonicalPath = routeObject.OLSKRoutePath;
+
+	var matches = routeObject.OLSKRoutePath.match(/(:[A-Za-z0-9_]*)/g);
+	if (matches) {
+		if (typeof optionalParams !== 'object' || optionalParams === null) {
+			throw new Error('OLSKErrorInputInvalid');
+		};
+
+		matches.forEach(function (e) {
+			if (!optionalParams[e.split(':').pop()]) {
+				throw new Error('OLSKErrorInputInvalid');
+			};
+
+			canonicalPath = canonicalPath.replace(e, optionalParams[e.split(':').pop()])
+		});
+	}
+
+	if (optionalParams) {
+		if (optionalParams.OLSKRoutingLanguage) {
+			canonicalPath = ['/', optionalParams.OLSKRoutingLanguage, canonicalPath].join('');
+		}
+	}
+
+	return canonicalPath;
+};
