1a42f0860644efa4686aa1dc33e2a20229b433d0
Rosano Coutinho <pub@xeari.com>
2019-12-12 17:51:14 -0500

    Tidy module

--- main.js
+++ main.js
@@ -1,127 +1,120 @@
-/*!
- * OLSKRouting
- * Copyright(c) 2018 Rosano Coutinho
- * MIT Licensed
- */
-
 (function(global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 		typeof define === 'function' && define.amd ? define(['exports'], factory) :
 			(factory((global.OLSKRouting = global.OLSKRouting || {})));
-}(this, (function(exports) {
-	'use strict';
+}(this, (function(exports) { 'use strict';
 
-//_ OLSKRoutingInputDataIsRouteObject
+	const mod = {
 
-exports.OLSKRoutingInputDataIsRouteObject = function(inputData) {
-	if (typeof inputData !== 'object' || inputData === null) {
-		return false;
-	}
-
-	if (typeof inputData.OLSKRoutePath !== 'string') {
-		return false;
-	}
-
-	if (inputData.OLSKRouteRedirect !== undefined) {
-		if (typeof inputData.OLSKRouteRedirect !== 'string') {
-			return false;
-		}
-
-		return true;
-	}
-
-	if (typeof inputData.OLSKRouteMethod !== 'string') {
-		return false;
-	}
-
-	if (typeof inputData.OLSKRouteFunction !== 'function') {
-		return false;
-	}
-
-	if (inputData.OLSKRouteSignature !== undefined) {
-		if (typeof inputData.OLSKRouteSignature !== 'string') {
-			return false;
-		}
-
-		if (inputData.OLSKRouteSignature.match(/\s/)) {
-			return false;
-		};
-	}
-
-	if (inputData.OLSKRouteIsHidden) {
-		if (typeof inputData.OLSKRouteIsHidden !== 'boolean') {
-			return false;
-		}
-	}
-
-	if (inputData.OLSKRouteMiddlewares) {
-		if (!Array.isArray(inputData.OLSKRouteMiddlewares)) {
-			return false;
-		}
-	}
-
-	return true;
-};
-
-//_ OLSKRoutingCanonicalPathWithRoutePathAndOptionalParams
-
-exports.OLSKRoutingCanonicalPathWithRoutePathAndOptionalParams = function(routePath, optionalParams = {}) {
-	if (typeof routePath !== 'string') {
-		throw new Error('OLSKErrorInputNotValid');
-	}
-
-	var canonicalPath = exports.OLSKRoutingSubstitutionFunctionForRoutePath(routePath)(optionalParams);
-
-	if (optionalParams && optionalParams.OLSKRoutingLanguage) {
-		canonicalPath = ['/', optionalParams.OLSKRoutingLanguage, canonicalPath].join('');
-
-		delete optionalParams.OLSKRoutingLanguage;
-	}
-
-	if (optionalParams && optionalParams.OLSKRoutingOrigin) {
-		canonicalPath = [optionalParams.OLSKRoutingOrigin, canonicalPath].join('');
-
-		delete optionalParams.OLSKRoutingOrigin;
-	}
-
-	const query = Object.keys(optionalParams).map(key => key + '=' + optionalParams[key]).join('&');
-
-	return canonicalPath + (query ? `?${ query }` : '');
-};
-
-//_ OLSKRoutingSubstitutionFunctionForRoutePath
-
-exports.OLSKRoutingSubstitutionFunctionForRoutePath = function(routePath) {
-	if (typeof routePath !== 'string') {
-		throw new Error('OLSKErrorInputNotValid');
-	}
-
-	var functionString = (
-		function(inputData) {
+		OLSKRoutingInputDataIsRouteObject (inputData) {
 			if (typeof inputData !== 'object' || inputData === null) {
-				throw new Error('OLSKErrorInputNotValidMissingInput');
+				return false;
 			}
 
-			var substitutedPath = 'OLSKRoutingSubstitutionFunctionTemplate';
+			if (typeof inputData.OLSKRoutePath !== 'string') {
+				return false;
+			}
 
-			(substitutedPath.match(/(:[\w]+(\(.*\))?)/g) || []).forEach(function(e) {
-				if (!inputData[e.split(':').pop().split('(').shift()]) {
-					throw new Error('OLSKErrorInputNotValidMissingRouteParam');
+			if (inputData.OLSKRouteRedirect !== undefined) {
+				if (typeof inputData.OLSKRouteRedirect !== 'string') {
+					return false;
 				}
 
-				substitutedPath = substitutedPath.replace(e, inputData[e.split(':').pop().split('(').shift()]);
+				return true;
+			}
 
-				delete inputData[e.split(':').pop().split('(').shift()]
-			});
+			if (typeof inputData.OLSKRouteMethod !== 'string') {
+				return false;
+			}
 
-			return substitutedPath;
-		}
-	).toString().replace('OLSKRoutingSubstitutionFunctionTemplate', routePath);
+			if (typeof inputData.OLSKRouteFunction !== 'function') {
+				return false;
+			}
 
-	var alfa;
-	eval('alfa = ' + functionString);
-	return alfa;
-};
+			if (inputData.OLSKRouteSignature !== undefined) {
+				if (typeof inputData.OLSKRouteSignature !== 'string') {
+					return false;
+				}
+
+				if (inputData.OLSKRouteSignature.match(/\s/)) {
+					return false;
+				};
+			}
+
+			if (inputData.OLSKRouteIsHidden) {
+				if (typeof inputData.OLSKRouteIsHidden !== 'boolean') {
+					return false;
+				}
+			}
+
+			if (inputData.OLSKRouteMiddlewares) {
+				if (!Array.isArray(inputData.OLSKRouteMiddlewares)) {
+					return false;
+				}
+			}
+
+			return true;
+		},
+
+		OLSKRoutingCanonicalPathWithRoutePathAndOptionalParams (routePath, optionalParams = {}) {
+			if (typeof routePath !== 'string') {
+				throw new Error('OLSKErrorInputNotValid');
+			}
+
+			var canonicalPath = exports.OLSKRoutingSubstitutionFunctionForRoutePath(routePath)(optionalParams);
+
+			if (optionalParams && optionalParams.OLSKRoutingLanguage) {
+				canonicalPath = ['/', optionalParams.OLSKRoutingLanguage, canonicalPath].join('');
+
+				delete optionalParams.OLSKRoutingLanguage;
+			}
+
+			if (optionalParams && optionalParams.OLSKRoutingOrigin) {
+				canonicalPath = [optionalParams.OLSKRoutingOrigin, canonicalPath].join('');
+
+				delete optionalParams.OLSKRoutingOrigin;
+			}
+
+			const query = Object.keys(optionalParams).map(key => key + '=' + optionalParams[key]).join('&');
+
+			return canonicalPath + (query ? `?${ query }` : '');
+		},
+
+		OLSKRoutingSubstitutionFunctionForRoutePath (routePath) {
+			if (typeof routePath !== 'string') {
+				throw new Error('OLSKErrorInputNotValid');
+			}
+
+			var functionString = (
+				function(inputData) {
+					if (typeof inputData !== 'object' || inputData === null) {
+						throw new Error('OLSKErrorInputNotValidMissingInput');
+					}
+
+					var substitutedPath = 'OLSKRoutingSubstitutionFunctionTemplate';
+
+					(substitutedPath.match(/(:[\w]+(\(.*\))?)/g) || []).forEach(function(e) {
+						if (!inputData[e.split(':').pop().split('(').shift()]) {
+							throw new Error('OLSKErrorInputNotValidMissingRouteParam');
+						}
+
+						substitutedPath = substitutedPath.replace(e, inputData[e.split(':').pop().split('(').shift()]);
+
+						delete inputData[e.split(':').pop().split('(').shift()]
+					});
+
+					return substitutedPath;
+				}
+			).toString().replace('OLSKRoutingSubstitutionFunctionTemplate', routePath);
+
+			var alfa;
+			eval('alfa = ' + functionString);
+			return alfa;
+		},
+		
+	};
+
+	Object.assign(exports, mod);
 
 	Object.defineProperty(exports, '__esModule', {
 		value: true
